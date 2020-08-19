a4007f4c41626ce2dc79beeeb8dd119088bc6bbf
Dutchy58 <54145413+Dutchy58@users.noreply.github.com>
2020-08-19 23:28:42 +0100

    Add files via upload

---
+++ java/HelloWorld.java
@@ -0,0 +1,11 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!!!");
+		System.out.println(StringUtils.capitalize("Hello World!!!"));
+	}
+
+}
